f67faf2cc8896626fc32a21fe528fc925e0c495c
MarjaCarty <marja.d.carty@gmail.com>
2020-10-08 14:09:29 -0500

    Style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import img from "../assets/header-01.png";
+
+const StyledHeader = styled.div`
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  padding: 11%;
+
+  h1,
+  h3 {
+    font-family: "Cinzel Decorative";
+    text-align: center;
+    color: #ffe0b5;
+    margin: 0;
+    position: relative;
+    bottom: 40px;
+  }
+
+  h1 {
+    font-size: 8rem;
+  }
+
+  h3 {
+    font-size: 4rem;
+  }
+`;
 
 export default function Header() {
   return (
-    <div className="Header">
+    <StyledHeader>
       <h1>NASA</h1>
       <h3>APOD</h3>
-    </div>
+    </StyledHeader>
   );
 }
