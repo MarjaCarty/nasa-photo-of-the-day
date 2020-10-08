6f451a394736442a020e1c2d851858c11db57c45
MarjaCarty <marja.d.carty@gmail.com>
2020-10-08 14:18:32 -0500

    Style Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  background-color: #171738;
+  width: 100%;
+  text-align: center;
+
+  i {
+    color: #ffe0b5;
+    padding: 3%;
+  }
+`;
 
 export default function Footer() {
   return (
-    <div className="Footer">
+    <StyledFooter>
       <i class="fab fa-instagram fa-3x"></i>
       <i class="fab fa-facebook-f fa-3x"></i>
       <i class="fab fa-twitter fa-3x"></i>
-    </div>
+    </StyledFooter>
   );
 }
